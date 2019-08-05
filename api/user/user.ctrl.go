2517a9946558e9b6051c59dc9edc3820cd21a373
fahmizzamanm <fahmizzamanm@garena.co.id>
2019-08-05 18:13:34 +0700

    finishing user register

--- api/user/user.ctrl.go
+++ api/user/user.ctrl.go
@@ -8,6 +8,7 @@ import (
 	"github.com/jinzhu/gorm"
 
 	"github.com/adifahmi/learn-gin/database/models"
+	"github.com/adifahmi/learn-gin/lib"
 )
 
 type User = models.User
@@ -27,11 +28,13 @@ func listUsers(c *gin.Context) {
 }
 
 func register(c *gin.Context) {
+	db := c.MustGet("db").(*gorm.DB)
+
 	type RequestBody struct {
-		Username string `form:"username" binding:"required,exists,alphanum,min=4,max=190"`
-		Email    string `form:"email" binding:"required,exists,email,max=190"`
-		Age      int    `form:"age" binding:"numeric"`
-		Password string `form:"password" binding:"required"`
+		Username string `json:"username" binding:"required,exists,alphanum,min=4,max=190"`
+		Email    string `json:"email" binding:"required,exists,email,max=190"`
+		Age      int    `json:"age"`
+		Password string `json:"password" binding:"required"`
 	}
 
 	var body RequestBody
@@ -43,7 +46,47 @@ func register(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{
-		"data": "ok",
+	// Check duplication
+	var exists User
+	if db.Where("username = ?", body.Username).Or("email = ?", body.Email).First(&exists).RecordNotFound() == false {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "username or email already exists",
+		})
+		return
+	}
+
+	hash, hashErr := lib.Hash(body.Password)
+	if hashErr != nil {
+		fmt.Println("hashErr", hashErr)
+		c.AbortWithStatus(500)
+		return
+	}
+
+	fmt.Println(hash)
+
+	// create user
+	user := User{
+		Username: body.Username,
+		Email:    body.Email,
+		Age:      body.Age,
+		Password: hash,
+	}
+
+	db.NewRecord(user)
+	db.Create(&user)
+
+	serialized := user.Serialize()
+	token, tokenErr := lib.GenerateToken(serialized, 7)
+	if tokenErr != nil {
+		fmt.Println("tokenErr", tokenErr)
+		c.AbortWithStatus(500)
+		return
+	}
+
+	c.SetCookie("token", token, 60*60*24*7, "/", "", false, true)
+
+	c.JSON(200, lib.JSON{
+		"user":  user.Serialize(),
+		"token": token,
 	})
 }
--- lib/common.go
+++ lib/common.go
@@ -1,4 +1,40 @@
 package lib
 
+import (
+	// "fmt"
+	jwt "github.com/dgrijalva/jwt-go"
+	"golang.org/x/crypto/bcrypt"
+	"os"
+	"time"
+)
+
 // JSON type
 type JSON = map[string]interface{}
+
+// Hash generate bcrypt of a string
+func Hash(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	return string(bytes), err
+}
+
+// CheckHash will check if hash is correct
+func CheckHash(password string, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+// GenerateToken will generate token from json
+func GenerateToken(data JSON, days int) (string, error) {
+
+	//  token is valid for X days
+	date := time.Now().Add(time.Hour * 24 * time.Duration(days))
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user": data,
+		"exp":  date.Unix(),
+	})
+
+	key := []byte(os.Getenv("SECRET_KEY"))
+	tokenString, err := token.SignedString(key)
+	return tokenString, err
+}
